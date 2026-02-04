eac5d389e991dfa1c5f2fa3b28b2cb627aee481f
Jyoti <pur583395@gmail.com>
2026-02-04 19:22:59 +0530

    baseurl update

--- ui/src/lib/api.js
+++ ui/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || '/api/v1'
+const baseURL = import.meta.env.VITE_API_URL 
 const api = axios.create({
   baseURL,
   headers: { 'Content-Type': 'application/json' }
